0fc65d7399302ab8e08f0453d0851628b7656fb7
Sam Suh <suhvivor24@gmail.com>
2022-11-12 16:59:27 -0800

    reverting main to safe state

--- react-app/src/components/BuySell/BuySellPage.js
+++ react-app/src/components/BuySell/BuySellPage.js
@@ -25,29 +25,6 @@ const randomString = crypto.randomBytes(32).toString('hex');
 
 
 const BuySellPage = ({setShowMain}) => {
-    const coinImg = {
-        "apecoin": "https://assets.coingecko.com/coins/images/24383/small/apecoin.jpg?1647476455",
-        "avalanche-2": "https://assets.coingecko.com/coins/images/12559/thumb/coin-round-red.png?1604021818",
-        "binancecoin": "https://assets.coingecko.com/coins/images/825/thumb/bnb-icon2_2x.png?1644979850",
-        "bitcoin": "https://assets.coingecko.com/coins/images/1/thumb/bitcoin.png?1547033579",
-        "binance-usd": "https://assets.coingecko.com/coins/images/9576/thumb/BUSD.png?1568947766",
-        "cardano": "https://assets.coingecko.com/coins/images/975/thumb/cardano.png?1547034860",
-        "dogecoin": "https://assets.coingecko.com/coins/images/5/thumb/dogecoin.png?1547792256",
-        "ethereum": "https://assets.coingecko.com/coins/images/279/thumb/ethereum.png?1595348880",
-        "eth2-staking-by-poolx": "https://assets.coingecko.com/coins/images/13853/thumb/5fc5b05df7b4c20006fb9fcb_eth_2.0-01.png?1612411843",
-        "litecoin": "https://assets.coingecko.com/coins/images/2/thumb/litecoin.png?1547033580",
-        "matic-network": "https://assets.coingecko.com/coins/images/4713/thumb/matic-token-icon.png?1624446912",
-        "near": "https://assets.coingecko.com/coins/images/10365/thumb/near_icon.png?1601359077",
-        "polkadot": "https://assets.coingecko.com/coins/images/12171/thumb/polkadot.png?1639712644",
-        "ripple": "https://assets.coingecko.com/coins/images/44/thumb/xrp-symbol-white-128.png?1605778731",
-        "shiba-inu": "https://assets.coingecko.com/coins/images/11939/thumb/shiba.png?1622619446",
-        "solana": "https://assets.coingecko.com/coins/images/4128/thumb/solana.png?1640133422",
-        "stellar": "https://assets.coingecko.com/coins/images/100/thumb/Stellar_symbol_black_RGB.png?1552356157",
-        "tether": "https://assets.coingecko.com/coins/images/325/thumb/Tether-logo.png?1598003707",
-        "tron": "https://assets.coingecko.com/coins/images/1094/thumb/tron-logo.png?1547035066",
-        "uniswap": "https://assets.coingecko.com/coins/images/12504/thumb/uniswap-uni.png?1600306604",
-        "usd-coin": "https://assets.coingecko.com/coins/images/6319/thumb/USD_Coin_icon.png?1547042389",
-    }
 
     const symbols = {
         "apecoin": "APE",
@@ -279,7 +256,6 @@ const BuySellPage = ({setShowMain}) => {
                 dispatch(getCurrentUserCards())
                 setShowEditModal(false)
                 return
-                // history.push('/') // redirect to home for now, change to user profile when created
             }
 
 
@@ -307,37 +283,13 @@ const BuySellPage = ({setShowMain}) => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        // setWalletAddress(currWallet[assetType].wallet_address)
         let value = cashValueCalculator(assetAmount, holdAssetPrice);
         let amount = amountCalculator(cashValue, holdAssetPrice)
 
-
-        // let test = '0.9291029'
-        // 
         let transaction;
 
-
-        // let value = assetAmount * allAssets[`${assetType}`]
         if (!transactionErrors.length) {
-            // setShowTransactionErrors(true)
 
-            // 
-            // if (assetAmount === null && assetType) setAssetAmount(Number(cashValue) / holdAssetPrice)
-            // if (cashValue === null && assetType) setCashValue(Number(assetAmount) * holdAssetPrice)
-
-            // if (assetAmount) {
-            //     setAssetAmount(String(assetAmount))
-            // } else {
-            //     setAssetAmount(String(amount))
-            // };
-
-            // if (cashValue) {
-            //     setCashValue(String(cashValue))
-            // } else {
-            //     setCashValue(String(value))
-            // };
-
-            // if they purchase using a crypto AMOUNT
             if (cashValue === null) {
                 transaction = {
                     transaction_type: transactionType,
@@ -374,31 +326,6 @@ const BuySellPage = ({setShowMain}) => {
                 }
             }
 
-            //save
-            // const transaction2 = {
-            //     transaction_type: transactionType,
-            //     asset_amount: (assetAmount ? +assetAmount : +amount),
-            //     cash_value: (cashValue ? +cashValue : +value),
-            //     asset_type: assetType,
-            //     card_id: card.id,
-            //     wallet_address: randomString,
-            //     // user_id: currUser.id
-            // } 
-            // // experiment
-            // if (currWallet[assetType] === transaction.asset_type) {
-            //     // run new transaction and update wallet route using transaction data.
-            //     // 1) dispatch create transaction thunk
-            //     // 2) query for existing wallet using transaction data in same create T route and
-            //     //  update using T form data
-            //     // Use const transaction
-            // } else {
-            //     // run new transaction2, create and update wallet
-            //     // 1) dispatch create transaction thunk
-            //     // 2) in new transaction thunk, create wallet using transaction2 data
-            //     // 3) therefore wallet is up to date
-            // }
-            // //experiment end
-            //save
 
             let checkWallet = await dispatch(checkWalletThunk(assetType))
             console.log('back to the good ol console logs',checkWallet)
