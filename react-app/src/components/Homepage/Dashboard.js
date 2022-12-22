d9125d5ee601c2e462f394e9e0475d64a2400cd2
Samsuhhh <suhvivor24@gmail.com>
2022-12-21 19:32:01 -0800

    pie chart colors fixed

--- react-app/src/components/Homepage/Dashboard.js
+++ react-app/src/components/Homepage/Dashboard.js
@@ -114,14 +114,18 @@ const Dashboard = () => {
                 backgroundColor: [
                     'rgba(255, 99, 132, 0.2)',
                     'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 205, 86, 0.2)'
+                    'rgba(255, 205, 86, 0.2)',
+                    'rgba(119, 221, 119, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
                 ],
                 borderColor: [
                     'rgba(255, 99, 132, 1)',
                     'rgba(54, 162, 235, 1)',
-                    'rgba(255, 205, 86, 1)'
+                    'rgba(255, 205, 86, 1)',
+                    'rgba(119, 221, 119, 1)',
+                    'rgba(75, 192, 192, 1)',
                 ],
-                hoverOffset: 4
+                hoverOffset: 8
             }]
         });
     }, [currWallet])
